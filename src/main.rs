146fcce4eb61a47acaaa7e3530f3901c48d0e2e1
Ulisse Mini <ulisse.mini@gmail.com>
2020-01-11 17:19:21 -0500

    pretty cool huh

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| App::new()
         .wrap(middleware::Logger::default())
-        .service(fs::Files::new("/", "./static").index_file("index.html")))
+        .service(fs::Files::new("/", "./public").index_file("index.html")))
         .bind("127.0.0.1:8080")?
         .run()
         .await
