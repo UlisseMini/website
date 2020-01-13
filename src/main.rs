6e893959e5330ea60b03ff89969229494f132350
Ulisse Mini <ulisse.mini@gmail.com>
2020-01-13 10:34:47 -0500

    Record IP and user agent

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_files as fs;
 use actix_web::{
-    middleware, web, App, HttpMessage, HttpRequest, HttpResponse, HttpServer, Responder, Error, error
+    middleware, web, App, HttpRequest, HttpResponse, HttpServer, Error, error
 };
 use env_logger;
 use futures::StreamExt;
@@ -10,10 +10,15 @@ use std::io::prelude::*;
 
 const MAX_SIZE: usize = 262_144; // max payload size is 16k
 
+fn get_user_agent<'a>(req: &'a HttpRequest) -> Option<&'a str> {
+        req.headers().get("User-Agent")?.to_str().ok()
+}
+
 // TODO: Add rate limiting
 // TODO: Make server errors nicer (currently frontend says 500 error not error message)
-// TODO: Record IP and Date of messages
-async fn message(mut payload: web::Payload) -> Result<HttpResponse, Error> {
+// TODO: Record Date of messages
+// TODO: Save messages in a more efficent way
+async fn message(mut payload: web::Payload, req: HttpRequest) -> Result<HttpResponse, Error> {
     let mut bytes = web::BytesMut::new();
     while let Some(chunk) = payload.next().await {
         let chunk = chunk?;
@@ -32,7 +37,12 @@ async fn message(mut payload: web::Payload) -> Result<HttpResponse, Error> {
         .create(true)
         .open("messages.txt")?;
 
-    bytes.extend_from_slice(b"\n-------------------------------------------------------\n");
+
+    let conn_info = req.connection_info();
+    let suffix = format!("\n\n{} | {}\n\n",
+        conn_info.remote().unwrap_or("???"),
+        get_user_agent(&req).unwrap_or("???"));
+    bytes.extend_from_slice(suffix.as_bytes());
     file.write_all(&bytes)?;
 
     Ok(HttpResponse::Ok().body("Submitted"))
