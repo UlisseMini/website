8e703da64412b48d2a2650a4cf99396181aee7eb
Ulisse Mini <ulisse.mini@gmail.com>
2020-01-15 16:04:31 -0500

    Refactor, add listenfd for hotreload

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,28 @@
 use actix_files as fs;
-use actix_web::{error, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web::{middleware, web, App, HttpServer};
 use env_logger;
-use futures::StreamExt;
+use listenfd::ListenFd;
 
-use std::fs::OpenOptions;
-use std::io::prelude::*;
-
-const MAX_SIZE: usize = 262_144; // max payload size is 16k
-
-fn get_user_agent<'a>(req: &'a HttpRequest) -> Option<&'a str> {
-    req.headers().get("User-Agent")?.to_str().ok()
-}
-
-fn make_suffix(req: HttpRequest) -> String {
-    use chrono::Utc;
-    use chrono_tz::America::New_York;
-
-    let date = Utc::now();
-
-    let conn_info = req.connection_info();
-
-    // TODO: Fix this, it is reporting incorrect time!
-    format!(
-        "addr: {}\nuser-agent: {}\ndate: {}\n",
-        conn_info.remote().unwrap_or("???"),
-        get_user_agent(&req).unwrap_or("???"),
-        date.with_timezone(&New_York)
-            .format("%D %I:%M:%S %p")
-            .to_string()
-    )
-}
-
-// TODO: Add rate limiting
-// TODO: Make server errors nicer (currently frontend says 500 error not error message)
-// TODO: Record Date of messages
-// TODO: Save messages in a more efficent way
-async fn message(mut payload: web::Payload, req: HttpRequest) -> Result<HttpResponse, Error> {
-    let mut bytes = web::BytesMut::new();
-    while let Some(chunk) = payload.next().await {
-        let chunk = chunk?;
-
-        // limit max size of in-memory payload
-        if (bytes.len() + chunk.len()) > MAX_SIZE {
-            return Err(error::ErrorBadRequest("overflow"));
-        }
-        bytes.extend_from_slice(&chunk);
-    }
-
-    // TODO: Make async
-    // TODO: Compress file when it reaches a specific size?
-    let mut file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open("messages.txt")?;
-
-    bytes.extend_from_slice(b"\n--------------------------------------\n");
-    file.write_all(&make_suffix(req).as_bytes())?;
-    file.write_all(&bytes)?;
-
-    Ok(HttpResponse::Ok().body("Submitted"))
-}
+mod message;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
-    HttpServer::new(|| {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
-            .route("/message", web::post().to(message))
+            .route("/message", web::post().to(message::message))
             .service(fs::Files::new("/", "./public").index_file("index.html"))
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    });
+
+        server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:8080")?
+    };
+
+    server.run().await
 }
