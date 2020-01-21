e58c9fc6f9c240c84c1f2edf084a441ccfc0c343
Ulisse Mini <ulisse.mini@gmail.com>
2020-01-21 16:50:26 -0500

    Updates to make it work with heroku

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ mod message;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
+
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
     env_logger::init();
 
     HttpServer::new(|| {
@@ -15,7 +21,7 @@ async fn main() -> std::io::Result<()> {
             .route("/message", web::post().to(message::message))
             .service(fs::Files::new("/", "./public").index_file("index.html"))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
